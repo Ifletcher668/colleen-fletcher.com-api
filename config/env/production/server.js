f960a3b66200550f79329e4eef831782750861c6
Isiah <Isiahfletcher7@gmail.com>
2021-01-04 09:41:15 -0700

    added secret to config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,12 @@
 module.exports = ({env}) => ({
     url: env('HEROKU_URL'),
+    cron: {
+        enabled: false,
+    },
+    admin: {
+        url: '/dashboard',
+        auth: {
+            secret: env('ADMIN_JWT_SECRET'),
+        },
+    },
 });

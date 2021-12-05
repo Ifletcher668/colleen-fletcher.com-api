7a8e8e02da54f8b928471c1f0e680044c81b253b
Isiah Fletcher <Isiahfletcher7@gmail.com>
2021-12-05 11:57:15 -0700

    add custom 'tilt' button

--- plugins/wysiwyg/admin/src/components/QuillEditor/index.js
+++ plugins/wysiwyg/admin/src/components/QuillEditor/index.js
@@ -1,27 +1,62 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+
+var tiltButton = document.querySelector('.ql-tilt');
+
+tiltButton.addEventListener('click', function() {
+        var range = quill.getSelection();
+        if(range){
+            console.log('range is valid');
+            quill.formatText(range,'tilt');
+        }else{
+            console.log('it it invalid');
+        }
+
+    }
+);
+
+let Inline = Quill.import('blots/inline');
+
+class Tilt extends Inline{    
+    
+    static create(value){
+        let node = super.create();
+        node.setAttribute('class','tilt');
+        return node;    
+    } 
+
+    
+}
+
+Tilt.blotName = 'tilt';
+Tilt.tagName = 'div';
+Quill.register(Tilt);
 
 const Editor = ({onChange, name, value}) => {
     const modules = {
         toolbar: [
-            [{header: [1, 2, 3, 4, 5, 6, false]}],
-            [{font: []}],
-            [{size: []}],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ font: [] }, { size: [] }, 'tilt'],
+            [],
             ['bold', 'italic', 'underline', 'strike', 'blockquote'],
             [
-                {list: 'ordered'},
-                {list: 'bullet'},
-                {indent: '-1'},
-                {indent: '+1'},
+                { list: 'ordered' },
+                { list: 'bullet' },
+                { indent: '-1' },
+                { indent: '+1' },
             ],
-            ['link'],
-            ['clean'],
+            ['link', 'clean'],
         ],
         clipboard: {
             // toggle to add extra line breaks when pasting HTML:
             matchVisual: false,
         },
+        history: {
+            delay: 2000,
+            maxStack: 500,
+            userOnly: true
+          }
     };
 
     const formats = [

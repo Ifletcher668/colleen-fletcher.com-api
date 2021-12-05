b989bcce4db91a1d652fb8be03cfbcb8ca8272db
Isiah Fletcher <Isiahfletcher7@gmail.com>
2021-12-05 16:56:56 -0700

    debug whitelist

--- plugins/wysiwyg/admin/src/components/QuillEditor/Modules/divider.js
+++ plugins/wysiwyg/admin/src/components/QuillEditor/Modules/divider.js
@@ -15,5 +15,5 @@ class Divider extends BlockEmbed {
 
 Divider.blotName = 'divider'
 Divider.tagName = 'p'
-Divider.whitelist = whitelist;
+// Divider.whitelist = whitelist;
 export default Divider;

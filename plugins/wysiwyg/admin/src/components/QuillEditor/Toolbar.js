4559f179b156b2950a83f388ea1e01f5835cbae1
Isiah Fletcher <Isiahfletcher7@gmail.com>
2021-12-05 14:38:52 -0700

    add custom toolbar

---
+++ plugins/wysiwyg/admin/src/components/QuillEditor/Toolbar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Section = ({ children }) => (
+    <span class="ql-formats">
+        {children}
+    </span>
+);
+
+const Toolbar = () => (
+    <div id="toolbar">
+        <Section>
+            <select className="ql-header" defaultValue={""} onChange={e => e.persist()}>
+                <option value="1" />
+                <option value="2" />
+                <option value="3" />
+                <option value="4" />
+                <option value="5" />
+                <option value="6" />
+                <option selected />
+            </select>
+
+            <select className="ql-font">
+                <option value="sans-serif" />
+                <option value="serif" />
+                <option value="mono" />
+            </select>
+
+            <select class="ql-size">
+                <option value="small" />
+                <option selected />
+                <option value="large" />
+                <option value="huge" />
+            </select>
+        </Section>
+
+        <Section>
+            <button className="ql-bold" />
+            <button className="ql-italic" />
+            <button className="ql-underline" />
+            <button className="ql-strike" />
+            <button className="ql-blockquote" />
+            <button className="ql-divider" />
+        </Section>
+
+        <Section>
+            <button class="ql-list" value="ordered" />
+            <button class="ql-list" value="bullet" />
+            <button class="ql-indent" value="-1" />
+            <button class="ql-indent" value="+1" />
+            <button class="ql-script" value="sub" />
+            <button class="ql-script" value="super" />
+        </Section>
+
+        <Section>
+            <button class="ql-link" />
+            <button class="ql-clean" />
+        </Section>
+    </div>
+  );
+
+export default Toolbar;
--- plugins/wysiwyg/admin/src/components/QuillEditor/index.js
+++ plugins/wysiwyg/admin/src/components/QuillEditor/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
 import './ToolbarButtons/Divider';
 import 'react-quill/dist/quill.snow.css';
 import './style.css';
+import Toolbar from './Toolbar';
 
 let Inline = Quill.import('blots/inline');
 
@@ -21,19 +22,18 @@ Quill.register(Tilt);
 
 const Editor = ({onChange, name, value}) => {
     const modules = {
-        toolbar: [
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-            [{ font: [] }, { size: [] }, 'tilt'],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote', 'divider'],
-            [
-                { list: 'ordered' },
-                { list: 'bullet' },
-                { indent: '-1' },
-                { indent: '+1' },
-            ],
-            ['link', 'clean'],
-        ],
-        
+        // toolbar: [
+        //     [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        //     [{ font: [] }, { size: [] }, 'tilt'],
+        //     ['bold', 'italic', 'underline', 'strike', 'blockquote', 'divider'],
+        //     [
+        //         { list: 'ordered' },
+        //         { list: 'bullet' },
+        //         { indent: '-1' },
+        //         { indent: '+1' },
+        //     ],
+        //     ['link', 'clean'],
+        // ],
         clipboard: {
             // toggle to add extra line breaks when pasting HTML:
             matchVisual: false,
@@ -65,15 +65,18 @@ const Editor = ({onChange, name, value}) => {
     ];
 
     return (
-        <ReactQuill
-            theme="snow"
-            value={value}
-            modules={modules}
-            formats={formats}
-            onChange={(content) => {
-                onChange({target: {name, value: content}});
-            }}
-        />
+        <Fragment>
+            <Toolbar />
+            <ReactQuill
+                theme="snow"
+                value={value}
+                modules={modules}
+                formats={formats}
+                onChange={(content) => {
+                    onChange({target: {name, value: content}});
+                }}
+            />
+        </Fragment>
     );
 };
 
